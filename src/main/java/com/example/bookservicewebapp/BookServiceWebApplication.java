6b8da8eacfc1731a5b73ac8004b7d3a9e8ee6b15
vnhk <47853604+vnhk@users.noreply.github.com>
2022-05-25 16:44:58 +0200

    Initial commit

---
+++ src/main/java/com/example/bookservicewebapp/BookServiceWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookservicewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookServiceWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookServiceWebApplication.class, args);
+    }
+
+}
