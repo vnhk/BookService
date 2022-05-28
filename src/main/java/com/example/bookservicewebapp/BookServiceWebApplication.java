89fbdb92ac067a6ab7524223b1628e61b71ee833
vnhk <47853604+vnhk@users.noreply.github.com>
2022-05-28 20:39:35 +0200

    Added tomcat deploy config

--- src/main/java/com/example/bookservicewebapp/BookServiceWebApplication.java
+++ src/main/java/com/example/bookservicewebapp/BookServiceWebApplication.java
@@ -2,12 +2,19 @@ package com.example.bookservicewebapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BookServiceWebApplication {
+public class BookServiceWebApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(BookServiceWebApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BookServiceWebApplication.class);
     }
 
 }
